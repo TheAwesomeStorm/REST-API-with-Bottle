86f5e309afc9e2c33a6a1f1381867d46dc25e661
Daniel Mendes Sechinske <90578340+TheAwesomeStorm@users.noreply.github.com>
2021-09-30 21:35:36 -0300

    Update singleton.py

--- src/libs/singleton.py
+++ src/libs/singleton.py
@@ -1,11 +1,11 @@
 class Singleton(type):
 
-    __instances = {}
+    _instances = {}
 
     def __call__(cls, *args, **kwargs):
 
-        if cls not in cls.__instances:
+        if cls not in cls._instances:
 
-            cls.__instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
 
-        return cls.__instances[cls]
+        return cls._instances[cls]
